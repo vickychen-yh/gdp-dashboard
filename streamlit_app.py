279f17333905c9ecd87019fd1a8476e6f7590b12
vickychen-yh <vicky.chen@wavein.com.tw>
2025-12-10 01:52:27 +0000

    edit title

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ import uvicorn
 
 # Set the title and favicon that appear in the Browser's tab bar.
 st.set_page_config(
-    page_title='GDP dashboard',
+    page_title='GDP dashboard test',
     page_icon=':earth_americas:', # This is an emoji shortcode. Could be a URL too.
 )
 
